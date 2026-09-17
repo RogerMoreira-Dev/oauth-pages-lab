5ee19f51b5150cdc135a121dcf81171f2fa56f8a
RogerMoreira-Dev <moreira.roger.costa@gmail.com>
2026-09-17 20:15:36 -0300

    Add OAuth login handling for Google and GitHub

--- functions/oauth/login/[provider].js
+++ functions/oauth/login/[provider].js
@@ -1,1 +1,15 @@
 
+import { randomToken, sha256Hex, sha256Base64url } from "../../_shared/crypto.js";
+import { setTxCookie } from "../../_shared/cookies.js";
+import { getProviderConfig } from "../../_shared/providers.js";
+
+export async function onRequestGet(context) {
+  const { env, params } = context;
+  const provider = params.provider;
+
+  if (provider !== "google" && provider !== "github") {
+    return new Response("Not found", { status: 404 });
+  }
+
+  const config = getProviderConfig(provider, env);
+  const baseUrl = env.PUBLIC_BASE_URL;
